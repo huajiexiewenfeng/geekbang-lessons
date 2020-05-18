41c4af56fc32caf28752dd7a8ed02f430a7fe822
huajiexiewenfeng <40154243+huajiexiewenfeng@users.noreply.github.com>
2020-05-18 20:39:39 +0800

    重新装载 Properties 对象，增加字符集
    重新装载 Properties 对象，增加字符集的逻辑，防止中文乱码

--- thinking-in-spring/i18n/src/main/java/org/geekbang/thinking/in/spring/i18n/DynamicResourceMessageSource.java
+++ thinking-in-spring/i18n/src/main/java/org/geekbang/thinking/in/spring/i18n/DynamicResourceMessageSource.java
@@ -125,7 +125,8 @@ public class DynamicResourceMessageSource extends AbstractMessageSource implemen
                                 // 处理为绝对路径
                                 Path filePath = dirPath.resolve(fileRelativePath);
                                 File file = filePath.toFile();
-                                Properties properties = loadMessageProperties(new FileReader(file));
+                                Reader reader = new InputStreamReader(new FileInputStream(file), ENCODING);
+                                Properties properties = loadMessageProperties(reader);
                                 synchronized (messageProperties) {
                                     messageProperties.clear();
                                     messageProperties.putAll(properties);
